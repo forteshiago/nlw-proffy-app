d7ebb3a05ce441ad3746d5f3a4b65c6721026976
forteshiago <forteshiago@hotmail.com>
2021-02-22 14:51:38 -0300

    Criada base para a pagina dos professores

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
 
 function TeacherList() {
     return(
-        <h1>Todos os teachers</h1>
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="voltar" />
+                    </Link>
+                    <img src={logoImg} alt="logo proffy" />
+                </div>
+                <div className="headercontent">
+                    <strong>Estes são os proffys disponíveis.</strong>
+                </div>
+            </header>
+        </div>
     )
 }
 
