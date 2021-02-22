2ef3f5628153e4b518f1763088e2fb12bb383a65
forteshiago <forteshiago@hotmail.com>
2021-02-22 16:00:43 -0300

    Criado componente header

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import PageHeader from "../../components/PageHeader";
 
-import logoImg from "../../assets/images/logo.svg";
-import backIcon from "../../assets/images/icons/back.svg";
+
+import "./styles.css";
 
 function TeacherList() {
     return(
         <div id="page-teacher-list" className="container">
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="voltar" />
-                    </Link>
-                    <img src={logoImg} alt="logo proffy" />
-                </div>
-                <div className="headercontent">
-                    <strong>Estes são os proffys disponíveis.</strong>
-                </div>
-            </header>
+            <PageHeader title="Estes são os proffys disponíveis." >
+                
+            </PageHeader>
         </div>
     )
 }
